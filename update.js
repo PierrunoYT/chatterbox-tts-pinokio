154ea2d543df34a8f4820b548c7425711ebfee40
PierrunoYT <pierrebruno@hotmail.ch>
2025-12-15 22:19:40 +0100

    Refactor update.js to remove redundant shell commands and streamline update process

--- update.js
+++ update.js
@@ -2,20 +2,8 @@ module.exports = {
   run: [{
     method: "shell.run",
     params: {
+      path: "GLM-TTS",
       message: "git pull"
-    }
-  }, {
-    method: "shell.run",
-    params: {
-      path: "app",
-      message: "git pull"
-    }
-  }, {
-    method: "shell.run",
-    params: {
-      venv: "env",
-      path: "app",
-      message: "uv pip install -r requirements.txt"
     }
   }]
 }
