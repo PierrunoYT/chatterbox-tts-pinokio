9317f2f67fc06eff7240e34bda5f20b32ebaf8e4
PierrunoYT <pierrebruno@hotmail.ch>
2025-12-15 21:53:34 +0100

    Refactor update.js to streamline shell commands for pulling updates and installing dependencies

--- update.js
+++ update.js
@@ -4,5 +4,18 @@ module.exports = {
     params: {
       message: "git pull"
     }
+  }, {
+    method: "shell.run",
+    params: {
+      path: "app",
+      message: "git pull"
+    }
+  }, {
+    method: "shell.run",
+    params: {
+      venv: "env",
+      path: "app",
+      message: "uv pip install -r requirements.txt"
+    }
   }]
 }
